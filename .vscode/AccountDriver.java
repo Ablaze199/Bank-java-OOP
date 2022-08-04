f19242c7fd823d607e1af195950389de1a28fe25
Ablaze99 <femiemma99@gmail.com>
2022-08-04 04:38:02 +0100

    bank-oop

--- .vscode/AccountDriver.java
+++ .vscode/AccountDriver.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
+
+import javax.sound.sampled.SourceDataLine;
+
 public class AccountDriver {
+    // Entry point of program
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        // create an array of Acounts
+        Account account[] = new Account[10];
+
+    }
+
+    public int menu(Scanner keyboard) {
+        System.out.println("Bank Account Menu");
+        System.out.println("1. Create NEW Account");
+        System.out.println("2. Deposit funds");
+        System.out.println("3. Withdraw funds");
+        System.out.println("4.Quit");
+
+    }
 
 }
